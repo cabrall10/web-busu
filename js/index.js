974338883f7e8bd1eaa02b06a97972fe283b8292
EnzoRobaina <enzo.robaina@gmail.com>
2018-12-14 13:21:25 -0200

    Adiciona função para iterar e adicionar classe ao slide

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $('.fp-tableCell', $('#paginaFeatures')).each(function () {
+        $(this).addClass("fp-tableCellCustom")
+        $(this).removeClass("fp-tableCell")
+    });
+    
+})
+
